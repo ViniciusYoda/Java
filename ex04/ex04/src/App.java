78cabbdfb85c39f2db60e840fdad15fb99f70737
Vinícius Yoda <vyoda4604@gmail.com>
2024-09-12 11:04:30 -0300

    Hello

---
+++ ex04/ex04/src/App.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Date relogio = new Date();]
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+}
